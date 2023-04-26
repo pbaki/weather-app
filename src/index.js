13afb39d75fcac46532bb670127ece628e2b920a
pbaki <pbaki999@gmail.com>
2023-04-26 18:57:53 +0200

    Configured webpack with plugins allowing bundling html and css

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./style.css";
+import { Key } from "./myKey";
+
+console.log(Key);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+    myKey: "./src/myKey.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Weather app",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+  devtool: "inline-source-map",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+};
