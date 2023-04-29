3a94a07665a9e4dcb45a111c5f632f998801de16
pbaki <pbaki999@gmail.com>
2023-04-29 15:13:29 +0200

    Initial api call test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import "./style.css";
 import { Key } from "./myKey";
 
-console.log(Key);
+async function apiRequest(location) {
+  try {
+    const request = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${Key}&q=${location}`,
+      {
+        mode: "cors",
+      }
+    );
+    const response = await request.json();
+    console.log(request);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+apiRequest("London");
