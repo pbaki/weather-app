5020d5a266a9df0cb0c232ca735aa2e60f00bc23
pbaki <pbaki999@gmail.com>
2023-05-17 19:55:34 +0200

    Adeed favicon fix

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Weather app",
       template: "./src/index.html",
+      favicon: "./src/Icons/cloud-icon.png",
     }),
     new MiniCssExtractPlugin({
       filename: "style.css",
